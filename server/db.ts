ae90c78b11b28adafa110329864767552a719139
Octa <ruarteoctavio8@gmail.com>
2022-10-05 17:22:18 -0300

    fix para deploy de las env

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,18 @@
 import * as admin from "firebase-admin";
+import { cert, initializeApp } from "firebase-admin/app";
+const isDev = process.env.NODE_ENV === "development";
 import * as serviceAccount from "../key.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: process.env.DATABASE_URL,
-});
+if (isDev) {
+  initializeApp({
+    credential: cert(serviceAccount as any),
+    databaseURL: process.env.DATABASE_URL,
+  });
+} else {
+  initializeApp({
+    credential: cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG || "")),
+    databaseURL: process.env.DATABASE_URL,
+  });
+}
 
 const firestore = admin.firestore();
 const rtdb = admin.database();
