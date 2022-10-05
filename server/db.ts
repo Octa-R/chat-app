512d413452b1bbfd51c80a43ddbc6388d70d6679
Octa <ruarteoctavio8@gmail.com>
2022-10-05 17:36:27 -0300

    api build error

--- server/db.ts
+++ server/db.ts
@@ -1,19 +1,9 @@
 import * as admin from "firebase-admin";
 import { cert, initializeApp } from "firebase-admin/app";
-const isDev = process.env.NODE_ENV === "development";
-import * as serviceAccount from "../key.json";
-if (isDev) {
-  initializeApp({
-    credential: cert(serviceAccount as any),
-    databaseURL: process.env.DATABASE_URL,
-  });
-} else {
-  initializeApp({
-    credential: cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG || "")),
-    databaseURL: process.env.DATABASE_URL,
-  });
-}
-
+initializeApp({
+  credential: cert(JSON.parse(process.env.FIREBASE_ADMIN_SDK_CONFIG || "")),
+  databaseURL: process.env.DATABASE_URL,
+});
 const firestore = admin.firestore();
 const rtdb = admin.database();
 
